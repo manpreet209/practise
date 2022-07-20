75b9891a2ee718dec0dd2e45da02609e935a027b
manpreet209 <98728203+manpreet209@users.noreply.github.com>
2022-07-20 14:38:37 +0530

    Add files via upload

---
+++ day 5.py
@@ -0,0 +1,4 @@
+a= 10
+while (a< 15):
+    a += 1
+    print('hello')
---
+++ day 6.py
@@ -0,0 +1,10 @@
+class A:
+    def m(self,a,b):
+        a= 10
+        b=20
+        print(a+b)
+
+ob= A()
+ob.m(5,15)
+ob1 = A()
+print(ob)
